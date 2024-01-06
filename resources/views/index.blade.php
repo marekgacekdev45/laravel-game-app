d372f547b77eb5512478a7b8232814856b7da49b
gacmen45 <marekgacek45@gmail.com>
2024-01-06 10:15:27 +0100

    style home page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,257 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mx-auto px-4">
+        <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular Games</h2>
+
+        <div class="popular-games text-sm grid grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#"><img src="{{ asset('/ff7.jpg') }}" alt="game cover"
+                            class="hover:opacity-75 transition ease-in-out duration-150"></a>
+                    <div class="absolute -bottom-4 -right-4 w-16 h-16 bg-gray-800 rounded-full">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full"> 80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8"> Final
+                    Fantasy 7 Remake</a>
+                <div class="text-gray-400 mt-1">PlayStation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#"><img src="{{ asset('/ff7.jpg') }}" alt="game cover"
+                            class="hover:opacity-75 transition ease-in-out duration-150"></a>
+                    <div class="absolute -bottom-4 -right-4 w-16 h-16 bg-gray-800 rounded-full">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full"> 80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8"> Final
+                    Fantasy 7 Remake</a>
+                <div class="text-gray-400 mt-1">PlayStation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#"><img src="{{ asset('/ff7.jpg') }}" alt="game cover"
+                            class="hover:opacity-75 transition ease-in-out duration-150"></a>
+                    <div class="absolute -bottom-4 -right-4 w-16 h-16 bg-gray-800 rounded-full">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full"> 80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8"> Final
+                    Fantasy 7 Remake</a>
+                <div class="text-gray-400 mt-1">PlayStation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#"><img src="{{ asset('/ff7.jpg') }}" alt="game cover"
+                            class="hover:opacity-75 transition ease-in-out duration-150"></a>
+                    <div class="absolute -bottom-4 -right-4 w-16 h-16 bg-gray-800 rounded-full">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full"> 80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8"> Final
+                    Fantasy 7 Remake</a>
+                <div class="text-gray-400 mt-1">PlayStation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#"><img src="{{ asset('/ff7.jpg') }}" alt="game cover"
+                            class="hover:opacity-75 transition ease-in-out duration-150"></a>
+                    <div class="absolute -bottom-4 -right-4 w-16 h-16 bg-gray-800 rounded-full">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full"> 80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8"> Final
+                    Fantasy 7 Remake</a>
+                <div class="text-gray-400 mt-1">PlayStation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#"><img src="{{ asset('/ff7.jpg') }}" alt="game cover"
+                            class="hover:opacity-75 transition ease-in-out duration-150"></a>
+                    <div class="absolute -bottom-4 -right-4 w-16 h-16 bg-gray-800 rounded-full">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full"> 80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8"> Final
+                    Fantasy 7 Remake</a>
+                <div class="text-gray-400 mt-1">PlayStation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#"><img src="{{ asset('/ff7.jpg') }}" alt="game cover"
+                            class="hover:opacity-75 transition ease-in-out duration-150"></a>
+                    <div class="absolute -bottom-4 -right-4 w-16 h-16 bg-gray-800 rounded-full">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full"> 80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8"> Final
+                    Fantasy 7 Remake</a>
+                <div class="text-gray-400 mt-1">PlayStation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#"><img src="{{ asset('/ff7.jpg') }}" alt="game cover"
+                            class="hover:opacity-75 transition ease-in-out duration-150"></a>
+                    <div class="absolute -bottom-4 -right-4 w-16 h-16 bg-gray-800 rounded-full">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full"> 80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8"> Final
+                    Fantasy 7 Remake</a>
+                <div class="text-gray-400 mt-1">PlayStation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#"><img src="{{ asset('/ff7.jpg') }}" alt="game cover"
+                            class="hover:opacity-75 transition ease-in-out duration-150"></a>
+                    <div class="absolute -bottom-4 -right-4 w-16 h-16 bg-gray-800 rounded-full">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full"> 80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8"> Final
+                    Fantasy 7 Remake</a>
+                <div class="text-gray-400 mt-1">PlayStation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#"><img src="{{ asset('/ff7.jpg') }}" alt="game cover"
+                            class="hover:opacity-75 transition ease-in-out duration-150"></a>
+                    <div class="absolute -bottom-4 -right-4 w-16 h-16 bg-gray-800 rounded-full">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full"> 80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8"> Final
+                    Fantasy 7 Remake</a>
+                <div class="text-gray-400 mt-1">PlayStation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#"><img src="{{ asset('/ff7.jpg') }}" alt="game cover"
+                            class="hover:opacity-75 transition ease-in-out duration-150"></a>
+                    <div class="absolute -bottom-4 -right-4 w-16 h-16 bg-gray-800 rounded-full">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full"> 80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8"> Final
+                    Fantasy 7 Remake</a>
+                <div class="text-gray-400 mt-1">PlayStation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#"><img src="{{ asset('/ff7.jpg') }}" alt="game cover"
+                            class="hover:opacity-75 transition ease-in-out duration-150"></a>
+                    <div class="absolute -bottom-4 -right-4 w-16 h-16 bg-gray-800 rounded-full">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full"> 80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8"> Final
+                    Fantasy 7 Remake</a>
+                <div class="text-gray-400 mt-1">PlayStation 4</div>
+            </div>
+
+        </div> <!--end popular games-->
+        <div class="flex my-10">
+            <div class="recently-reviewed w-3/4 mr-32">
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently viewed</h2>
+                <div class="recently-reviewed-container space-y-12 mt-8 ">
+
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#"><img src="{{ asset('/ff7.jpg') }}" alt="game cover"
+                                    class=" w-48 hover:opacity-75 transition ease-in-out duration-150"></a>
+                            <div class="absolute -bottom-4 -right-4 w-16 h-16 bg-gray-900 rounded-full">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full"> 80%</div>
+                            </div>
+                        </div>
+                        <div class="ml-12"> <a href="#"
+                                class="block text-large font-semibold leading-tight hover:text-gray-400 mt-4"> Final
+                                Fantasy 7 Remake</a>
+                            <div class="text-gray-400 mt-1">PlayStation 4</div>
+                            <p class="mt-6 text-gray-400">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae perferendis, repellendus
+                                architecto quasi reprehenderit voluptatibus magni laborum excepturi quam. Aut, blanditiis
+                                aspernatur eum inventore, cupiditate quos repudiandae mollitia ducimus, vitae rem soluta
+                                reprehenderit repellendus provident sapiente beatae nobis molestias! Excepturi?
+                            </p>
+                        </div>
+                    </div>
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#"><img src="{{ asset('/ff7.jpg') }}" alt="game cover"
+                                    class=" w-48 hover:opacity-75 transition ease-in-out duration-150"></a>
+                            <div class="absolute -bottom-4 -right-4 w-16 h-16 bg-gray-900 rounded-full">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full"> 80%</div>
+                            </div>
+                        </div>
+                        <div class="ml-12"> <a href="#"
+                                class="block text-large font-semibold leading-tight hover:text-gray-400 mt-4"> Final
+                                Fantasy 7 Remake</a>
+                            <div class="text-gray-400 mt-1">PlayStation 4</div>
+                            <p class="mt-6 text-gray-400">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae perferendis, repellendus
+                                architecto quasi reprehenderit voluptatibus magni laborum excepturi quam. Aut, blanditiis
+                                aspernatur eum inventore, cupiditate quos repudiandae mollitia ducimus, vitae rem soluta
+                                reprehenderit repellendus provident sapiente beatae nobis molestias! Excepturi?
+                            </p>
+                        </div>
+                    </div>
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#"><img src="{{ asset('/ff7.jpg') }}" alt="game cover"
+                                    class=" w-48 hover:opacity-75 transition ease-in-out duration-150"></a>
+                            <div class="absolute -bottom-4 -right-4 w-16 h-16 bg-gray-900 rounded-full">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full"> 80%</div>
+                            </div>
+                        </div>
+                        <div class="ml-12"> <a href="#"
+                                class="block text-large font-semibold leading-tight hover:text-gray-400 mt-4"> Final
+                                Fantasy 7 Remake</a>
+                            <div class="text-gray-400 mt-1">PlayStation 4</div>
+                            <p class="mt-6 text-gray-400">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae perferendis, repellendus
+                                architecto quasi reprehenderit voluptatibus magni laborum excepturi quam. Aut, blanditiis
+                                aspernatur eum inventore, cupiditate quos repudiandae mollitia ducimus, vitae rem soluta
+                                reprehenderit repellendus provident sapiente beatae nobis molestias! Excepturi?
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="most-anticipated w-1/4 ">
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently viewed</h2>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#"><img src="{{ asset('/cyberpunk.jpg') }}" alt="game cover"
+                            class=" w-16 hover:opacity-75 transition ease-in-out duration-150"></a>
+                            <div class="ml-4">
+                                <a href="#" class="hover:text-gray-300">Cyberpunk 2077</a>
+                                <div class="text-gray-400 text-sm mt-1">Sept.16 2020</div>
+                            </div>
+                    </div>
+                    <div class="game flex">
+                        <a href="#"><img src="{{ asset('/cyberpunk.jpg') }}" alt="game cover"
+                            class=" w-16 hover:opacity-75 transition ease-in-out duration-150"></a>
+                            <div class="ml-4">
+                                <a href="#" class="hover:text-gray-300">Cyberpunk 2077</a>
+                                <div class="text-gray-400 text-sm mt-1">Sept.16 2020</div>
+                            </div>
+                    </div>
+                    <div class="game flex">
+                        <a href="#"><img src="{{ asset('/cyberpunk.jpg') }}" alt="game cover"
+                            class=" w-16 hover:opacity-75 transition ease-in-out duration-150"></a>
+                            <div class="ml-4">
+                                <a href="#" class="hover:text-gray-300">Cyberpunk 2077</a>
+                                <div class="text-gray-400 text-sm mt-1">Sept.16 2020</div>
+                            </div>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+
+
+    </div> <!--end container-->
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GameApp</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-gray-900 text-white ">
+    <header class="border-b border-gray-800">
+        <nav class="container mx-auto flex items-center justify-between px-4 py-6">
+            <div class="flex items-center">
+                <a href="/">
+                    <img src="{{ asset('/logo.webp') }}" alt="logo" class="w-12 flex-none"></a>
+                <ul class="flex ml-16 space-x-8">
+                    <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
+                </ul>
+            </div>
+            <div class="flex items-center">
+                <div class="relative">
+                    <input type="text" class="bg-gray-800 text-sm rounded-full px-3 pl-8 py-1 w-64 "
+                        placeholder="search...">
+                    <div class="absolute top-0 flex items-center h-full ml-2">
+                        <svg class="fill-white w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960 ">
+                            <path
+                                d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+                        </svg>
+                    </div>
+                </div>
+                <div class="ml-6">
+                    <a href="#"><img src="{{ asset('avatar.jpg') }}" alt="avatar" class="rounded-full w-8"></a>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main class="py-8">
+       @yield('content')
+    </main>
+
+
+    <footer class="border-t border-gray-800">
+        <div class="container mx-auto px-4 py-6">
+            Powered by <a href="#" class="underline hover:text-gray-400"> IGDB API</a>
+        </div>
+    </footer>
+</body>
+
+</html>
