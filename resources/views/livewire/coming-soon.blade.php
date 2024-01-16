830d9ad2b1814314a70e60e1a96e4d6098b2f127
gacmen45 <marekgacek45@gmail.com>
2024-01-16 10:39:24 +0100

    make components

--- resources/views/livewire/coming-soon.blade.php
+++ resources/views/livewire/coming-soon.blade.php
@@ -1,14 +1,6 @@
 <div wire:init='loadComingSoon' class="most-anticipated-container space-y-10 mt-8">
     @forelse($comingSoon as $game)
-    <div class="game flex">
-        <a href="{{ route('games.show', $game['slug']) }}"><img src="{{$game['coverImgUrl']}}" alt="game cover"
-            class=" w-16 hover:opacity-75 transition ease-in-out duration-150"></a>
-            <div class="ml-4">
-                <a href="{{ route('games.show', $game['slug']) }}" class="hover:text-gray-300">{{$game['name']}}</a>
-                <div class="text-gray-400 text-sm mt-1">
-                    {{$game['releaseDate']}}</div>
-            </div>
-    </div> 
+    <x-game-card-small :game="$game"/>
   @empty
     @foreach (range(1, 4) as $game)
         <div class="game flex">
